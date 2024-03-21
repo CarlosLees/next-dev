65e5e4aea7500ced4e103686486e926a4666d630
Carlos <61411632+CarlosLees@users.noreply.github.com>
2024-03-21 13:55:21 +0800

    init project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,35 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    darkMode: 'class',
+    content: {
+        files: [
+            './src/app/**/*.{ts,tsx}',
+            './src/components/**/*.{ts,tsx}',
+            './src/lib/**/*.{ts,tsx}',
+        ],
     },
-  },
-  plugins: [],
+    theme: {
+        screens: {
+            sm: { max: '767px' },
+
+            md: { max: '1023px' },
+
+            lg: { max: '1279px' },
+
+            xl: { max: '1535px' },
+
+            '2xl': { min: '1536px' },
+        },
+        extend: {
+            flex: {
+                '2': '2 1 0%',
+            },
+        },
+    },
+    plugins: [],
+    corePlugins: {
+        preflight: false,
+    },
 };
 export default config;
